c67ae3eec915adfc80bdd7b817c24dd59b070455
Luzil7 <alvaroasc98@gmail.com>
2024-06-16 12:40:56 +0200

    API Key is read from a .txt file now

--- valorantFDS.py
+++ valorantFDS.py
@@ -2,37 +2,46 @@ import sys
 from valo_api import set_api_key
 import requests
 import json
+from os.path import dirname, abspath
 
-def get_leaderboard(region: str):
+general_url = "https://api.henrikdev.xyz/valorant/"
+
+def get_lifetime_matches(region: str,name: str,tag: str,mode: str=None,map: str=None,page: int=None,size: int=None):
     """
-    Get Valorant leaderboard information.
+    Get Valorant lifetime matches stat for a player.
 
     Parameters:
-        region (str): The Valorant region.
-
+        region  (str):  The Valorant region.
+        name    (str):  The player name.
+        tag     (str):  The player tag.
+        mode    (str):  Game mode of the matches. Options: competitive unrated deathmatch ... OPTIONAL
+        map     (str):  The map name. Starts with capital letters. OPTIONAL
+        page    (int):  Page size used for indentation. OPTIONAL
+        size    (int):  Number of returned matches. OPTIONAL 
     Returns:
-        Response: The HTTP response containing leaderboard information.
+        Response: The HTTP response containing the player matches.
     """
 
-    url = f"https://api.henrikdev.xyz/valorant/v1/leaderboard/{region}"
+    url = general_url + f"v1/lifetime/matches/{region}/{name}/{tag}"
+    params = {'mode': mode, 'map': map, 'page': page, 'size': size}
     api_key = get_api_key()
     headers = {
         "Authorization": api_key,
     }
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers,params=params)
         response.raise_for_status()
         return response
     except requests.exceptions.HTTPError as http_err:
         return http_err.response
     except requests.exceptions.RequestException as req_err:
-        print(f"Error retrieving Valorant leaderboard information: {req_err}")
+        print(f"Error retrieving matches: {req_err}")
         return None
     
 def get_api_key():
     """
-    Get API Key to be used in any request.
+    Get API Key to be used in any request. It must be stored in ValorantAPIKey.txt
 
     Parameters:
         void
@@ -40,14 +49,19 @@ def get_api_key():
     Returns:
         Response: API Key.
     """
-    return "HDEV-d7772cbc-223c-4593-8cb6-2345e30f3de3"  #To Do: read this from file for privacy reasons)
+    ws_path = dirname(abspath(__file__))
+    path = ws_path + "/ValorantAPIKey.txt"
+    apiKeyFile = open(path,"r")
+    apiKey = apiKeyFile.read()
+    return apiKey
 
 def main():
-    #name = "SpaguettiCoded"
-    #tag = "EUW"
-    #version = "08.11.00.2606987" 
-    leaderboard_request = get_leaderboard(region="na")
-    jsonArchive = leaderboard_request.json()
+    name = "SpaguettiCoded"
+    tag = "EUW"
+    matches_request = get_lifetime_matches(region="eu",name=name,tag=tag)
+    print(matches_request)
+    print(matches_request.url)
+    jsonArchive = matches_request.json()
     with open ("archivo.json","w") as j:
         json.dump(jsonArchive,j)
 
