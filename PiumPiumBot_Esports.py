7bfdac962750ff9e86e8cc7519a781c47d8ce7e7
Luzil7 <alvaroasc98@gmail.com>
2024-06-27 12:05:42 +0200

    Commands grouped in cogs

---
+++ PiumPiumBot_Esports.py
@@ -0,0 +1,46 @@
+from discord.ext import commands
+from valorantFDS import get_vct
+
+class Esports(commands.Cog):
+    "Commands related to Esports competitions"
+    @commands.command(name='emea')
+    async def get_emea(self, ctx):
+        "Informacion de la VCT EMEA. Resultados de los partidos disputados y calendario proximo"
+        response = get_vct("vct_emea")
+        await ctx.send(response)
+
+    @commands.command(name='na')
+    async def get_na(self, ctx):
+        "Informacion de la VCT Americas. Resultados de los partidos disputados y calendario proximo"
+        response = get_vct("vct_americas")
+        await ctx.send(response)
+
+    @commands.command(name='koi')
+    async def get_koi(self, ctx):
+        "Informacion de KOI. Resultados de los partidos disputados y calendario proximo"
+        response = get_vct("vct_emea", "KOI")
+        await ctx.send(response)
+
+    @commands.command(name='th')
+    async def get_th(self, ctx):
+        "Informacion de Heretics. Resultados de los partidos disputados y calendario proximo"
+        response = get_vct("vct_emea", "TH")
+        await ctx.send(response)
+
+    @commands.command(name='gx')
+    async def get_gx(self, ctx):
+        "Informacion de GiantX. Resultados de los partidos disputados y calendario proximo"
+        response = get_vct("vct_emea", "GX")
+        await ctx.send(response)
+
+    @commands.command(name='nrg')
+    async def get_nrg(self, ctx):
+        "Informacion de NRG. Resultados de los partidos disputados y calendario proximo"
+        response = get_vct("vct_americas", "NRG")
+        await ctx.send(response)
+
+    @commands.command(name='sen')
+    async def get_sen(self, ctx):
+        "Informacion de Sentinels. Resultados de los partidos disputados y calendario proximo"
+        response = get_vct("vct_americas", "SEN")
+        await ctx.send(response)
